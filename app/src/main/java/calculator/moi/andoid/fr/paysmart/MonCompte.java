7ce57065f5ce58a336b380677e59001d3a4eab67
JEREMY\jerem <jeremy.bouhi@edu.ece.fr>
2018-03-21 18:17:44 +0100

    mise en place des premiers styles, màj des colors dans les values

--- app/src/main/java/calculator/moi/andoid/fr/paysmart/MonCompte.java
+++ app/src/main/java/calculator/moi/andoid/fr/paysmart/MonCompte.java
@@ -2,6 +2,7 @@ package calculator.moi.andoid.fr.paysmart;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by jerem on 21/03/2018.
@@ -13,5 +14,11 @@ public class MonCompte extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mon_compte);
+
+        TextView solde = (TextView) findViewById(R.id.textView2);
+        solde.setText("Solde Total");
+
+        TextView varSolde = (TextView) findViewById(R.id.textView3);
+        varSolde.setText("50€");
     }
 }
