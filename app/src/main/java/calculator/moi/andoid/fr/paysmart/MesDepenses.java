0902d7dd090af1764a40257dc13da289d3806828
Clément Rocher <clementrocher71@gmail.com>
2018-03-22 12:18:09 +0100

    Page Depenses

---
+++ app/src/main/java/calculator/moi/andoid/fr/paysmart/MesDepenses.java
@@ -0,0 +1,13 @@
+package calculator.moi.andoid.fr.paysmart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MesDepenses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mes_depenses);
+    }
+}
