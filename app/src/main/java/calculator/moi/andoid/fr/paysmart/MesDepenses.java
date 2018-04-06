510505042d35a1e1fa31195d0ef0053b33e99521
Clément Rocher <clementrocher71@gmail.com>
2018-04-06 15:13:55 +0200

    Debug intensif

--- app/src/main/java/calculator/moi/andoid/fr/paysmart/MesDepenses.java
+++ app/src/main/java/calculator/moi/andoid/fr/paysmart/MesDepenses.java
@@ -1,9 +1,9 @@
 package calculator.moi.andoid.fr.paysmart;
 
-<<<<<<< HEAD
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by jerem on 26/03/2018.
@@ -22,16 +22,5 @@ public class MesDepenses extends FragmentActivity{
 
     }
 }
-=======
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MesDepenses extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.mes_depenses);
-    }
-}
->>>>>>> 27eeedbfe13ff78fe6aea042744a1729203d73a8
