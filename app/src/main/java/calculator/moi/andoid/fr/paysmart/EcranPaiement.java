189737b5e1b5f62d72ebd16fedbb93eb159535f1
JEREMY\jerem <jeremy.bouhi@edu.ece.fr>
2018-04-11 00:25:57 +0200

    teste la caméra

--- app/src/main/java/calculator/moi/andoid/fr/paysmart/EcranPaiement.java
+++ app/src/main/java/calculator/moi/andoid/fr/paysmart/EcranPaiement.java
@@ -1,5 +1,6 @@
 package calculator.moi.andoid.fr.paysmart;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
@@ -11,17 +12,26 @@ import android.hardware.Camera;
 
 import android.graphics.PixelFormat;
 import android.net.Uri;
+import android.os.Vibrator;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.File;
 import java.io.IOException;
@@ -31,14 +41,115 @@ public class EcranPaiement extends Fragment {
     static final int REQUEST_TAKE_PHOTO = 1;
     //static final int REQUEST_IMAGE_CAPTURE = 1;
 
+    SurfaceView cameraPreview;
+    BarcodeDetector barcodeDetector;
+    CameraSource cameraSource;
+    final int RequestCameraPermissionID = 1001;
+    TextView txtResult;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode)
+        {
+            case RequestCameraPermissionID:
+            {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    try {
+                        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                            // TODO: Consider calling
+                            cameraSource.start(cameraPreview.getHolder());
+                            return;
+                        }
+
+                    } catch (IOException e) {
+                        Log.d("ERROR","ça marche ap");
+                        e.printStackTrace();
+                    }
+                }
+            }
+            break;
+        }
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.ecran_paiement, container, false);
 
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+        //pour ouvrir le tel direct
+        //Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+
+        cameraPreview = (SurfaceView) view.findViewById(R.id.camera_preview);
+        txtResult = (TextView) view.findViewById(R.id.result);
+
+        barcodeDetector = new BarcodeDetector.Builder(getActivity())
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+
+        cameraSource = new CameraSource
+                .Builder(getActivity(), barcodeDetector)
+                .build();
+
+        cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+
+                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    ActivityCompat.requestPermissions(getActivity(),
+                            new String[]{Manifest.permission.CAMERA}, RequestCameraPermissionID);
+
+                    return;
+                }
+                    try {
+                        cameraSource.start(cameraPreview.getHolder());
+
+                    } catch (IOException e) {
+
+                        e.printStackTrace();
+                    }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>(){
+
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrcodes = detections.getDetectedItems();
+                if(qrcodes.size() != 0)
+                {
+                    txtResult.post(new Runnable(){
+
+                        @Override
+                        public void run() {
+                            //create vibrate
+                            Vibrator vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+                            vibrator.vibrate(1000);
+                            txtResult.setText(qrcodes.valueAt(0).displayValue);
+                        }
+                    });
+                }
+            }
+        });
 
         return view;
     }
