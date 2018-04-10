c3a1f54a0f72747adfed640455299393a6562690
JEREMY\jerem <jeremy.bouhi@edu.ece.fr>
2018-04-10 15:48:12 +0200

    ouverture de l'appareil photo dès qu'on clique sur Payer

--- app/src/main/java/calculator/moi/andoid/fr/paysmart/EcranPaiement.java
+++ app/src/main/java/calculator/moi/andoid/fr/paysmart/EcranPaiement.java
@@ -5,7 +5,11 @@ import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Camera;
+
+//import android.graphics.Camera;
+import android.hardware.Camera;
+
+import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -24,18 +28,17 @@ import java.io.IOException;
 
 public class EcranPaiement extends Fragment {
 
-    //static final int REQUEST_TAKE_PHOTO = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
     //static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    Camera camera; // camera class variable
-    SurfaceView camView; // drawing camera preview using this variable
-    SurfaceHolder surfaceHolder; // variable to hold surface for surfaceView which means display
-    boolean camCondition = false;  // conditional variable for camera preview checking and set to false
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.ecran_paiement, container, false);
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
 
         return view;
     }
